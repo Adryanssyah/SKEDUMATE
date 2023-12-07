19c03d3cb690f2df536f3ddba9c964b08878f599
Adryanssyah <adryansyah434@gmail.com>
2023-12-07 23:01:25 +0700

    When change route always scroll to top

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,13 @@ import checkAnggota from '../composables/checkAnggota';
 
 const router = createRouter({
      history: createWebHistory(import.meta.env.BASE_URL),
+     scrollBehavior(to, from, savedPosition) {
+          if (savedPosition) {
+               return savedPosition;
+          } else {
+               return { top: 0 };
+          }
+     },
      routes: [
           {
                path: '/',
