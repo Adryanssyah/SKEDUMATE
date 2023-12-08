899bf2f3866339d6c0be3f4334103e9c17464339
Adryanssyah <adryansyah434@gmail.com>
2023-12-08 14:30:00 +0700

    link to my new portfolio 🎉

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,7 +2,7 @@
      <footer class="w-full bg-white dark:bg-dark flex justify-center dark:border-dark-2 border-t-2 px-5">
           <div class="w-full max-w-[1100px] flex flex-col">
                <div class="flex flex-col items-center justify-center gap-7 md:flex-row md:justify-between md:items-center py-6">
-                    <a href="https://adryanssyah.github.io/portfolio/" target="_blank" class="font-medium dark:text-gray-400 dark:hover:text-white text-gray-600 hover:text-black text-sm">© 2023 Adryans</a>
+                    <a href="https://adryansportfolio.vercel.app/" target="_blank" class="font-medium dark:text-gray-400 dark:hover:text-white text-gray-600 hover:text-black text-sm">© 2023 Adryans</a>
                     <div class="text-lg flex gap-5">
                          <a class="dark:text-gray-400 dark:hover:text-white text-gray-500 hover:text-black" href="https://www.linkedin.com/in/muhammad-adryan-9322281a8/" target="_blank" aria-label="Kunjungi LinkedIn Saya">
                               <i class="bi bi-linkedin"></i
