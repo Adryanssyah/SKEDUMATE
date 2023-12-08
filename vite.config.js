062a100a1323aaac8a6886f61e3b50a633ef68e4
Adryanssyah <adryansyah434@gmail.com>
2023-12-08 15:56:19 +0700

    add pwa feature

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,40 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-     plugins: [vue(), vueJsx()],
+     plugins: [
+          vue(),
+          vueJsx(),
+          VitePWA({
+               manifest: {
+                    icons: [
+                         {
+                              src: '/logo.webp',
+                              sizes: '512x512',
+                              type: 'image/webp',
+                              purpose: 'any maskable',
+                         },
+                    ],
+               },
+               workbox: {
+                    runtimeCaching: [
+                         {
+                              urlPattern: /^https:\/\/skedumate-server\.vercel\.app\//,
+                              handler: 'CacheFirst',
+                              options: {
+                                   cacheName: 'skedumate-cache',
+                                   cacheableResponse: {
+                                        statuses: [0, 200],
+                                   },
+                              },
+                         },
+                    ],
+               },
+          }),
+     ],
      resolve: {
           alias: {
                '@': fileURLToPath(new URL('./src', import.meta.url)),
